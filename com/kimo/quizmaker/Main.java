0a3f07caa573e3ca3aed586b6406ef182656419a
Kareem Ghazi <k.ghazi2005@gmail.com>
2021-07-27 16:58:11 +0300

    Update Main.java

--- com/kimo/quizmaker/Main.java
+++ com/kimo/quizmaker/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Application application = new Application();
-		
+
 		application.printSeparator();
-		System.out.println("Quiz Maker - Creates randomized quizzes!");
+		System.out.println("Quiz Maker - Creates quizzes!");
 		application.printCommandList();
 		application.mainLoop();
 		application.printSeparator();
-		
+
 	}
 
 }
