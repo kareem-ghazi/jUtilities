cb18470dc9733549ef0e146cdda135597d6bb428
Kareem Ghazi <k.ghazi2005@gmail.com>
2021-08-05 16:00:45 +0300

    Add base logic for option 2

--- src/master/java/com/kimo/beginner/agecalculator/Main.java
+++ src/master/java/com/kimo/beginner/agecalculator/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 	static Scanner scan = new Scanner(System.in);
 	static Application application = new Application();
+	static User user = new User();
 
 	// Main method to run application.
 	public static void main(String[] args) {
@@ -22,65 +23,78 @@ public class Main {
 		System.out.println("2. Print out a summary about you using your birthdate.");
 		System.out.println("3. Print out your age on different planets.");
 		System.out.println("4. Exit.");
-		
+
 	}
 
 	// Prints a separator between program sessions.
 	private static void printSeparator() {
 		System.out.println("-------------------------------------");
-		
+
 	}
 
 	// Prints out the calculated age using the specified input values.
 	public static void mainLoop() {
 		int userInput = 0;
-		
+
 		do {
 			System.out.print("-: ");
 			try {
 				userInput = scan.nextInt();
 			} catch (Exception e) {
 				scan.nextLine();
-				System.err.println("Invalid input");
-				
 			}
-			
+
 			switch (userInput) {
-			case 1:
-				User user1 = new User();
-				
-				System.out.print("Enter your name: ");
-				String username = scan.nextLine();
-				
-				System.out.print("Enter your nationality: ");
-				String nationality = scan.nextLine();
-				
-				System.out.print("Enter your birth year: ");
-				int birthYear = scan.nextInt();
+				case 1:
+					scan.nextLine();
+					System.out.print("Enter your name: ");
+					String username = scan.nextLine();
 
-				System.out.print("Enter your birth month: ");
-				int birthMonth = scan.nextInt();
+					System.out.print("Enter your nationality: ");
+					String nationality = scan.nextLine();
 
-				System.out.print("Enter your birth day: ");
-				int birthDay = scan.nextInt();
-				scan.nextLine();
-				
-				user1.setName(username);
-				user1.setNationality(nationality);
-				user1.setBirthdate(birthYear, birthMonth, birthDay);
-				
-				System.out.println(user1.getUsername());
-				System.out.println(user1.getNationality());
-				break;
-				
-			case 4:
-				return;
-				
+					System.out.print("Enter your birth year: ");
+					int birthYear = scan.nextInt();
+
+					System.out.print("Enter your birth month: ");
+					int birthMonth = scan.nextInt();
+
+					System.out.print("Enter your birth day: ");
+					int birthDay = scan.nextInt();
+					scan.nextLine();
+
+					user.setName(username);
+					user.setNationality(nationality);
+					user.setBirthdate(birthYear, birthMonth, birthDay);
+
+					break;
+				case 2:
+					printSeparator();
+					if (user.getUsername() != null) {
+						System.out.println("Name: " + user.getUsername());
+						System.out.println("Nationality: " + user.getNationality());
+						System.out.println("Birthdate: " + user.getBirthYear() + "/" + user.getBirthMonth() + "/"
+								+ user.getBirthDay());
+						System.out.println("Age: ");
+						System.out.println("Time till your next birthday: ");
+						
+					} else {
+						System.err.println("You have not set your information yet.");
+						
+					}
+					break;
+				case 3:
+
+				case 4:
+					return;
+				default:
+					System.err.println("Invalid input.");
+
 			}
-			
+
 			printSeparator();
 			printCommandList();
-			
+
 		} while (true);
 
 	}
