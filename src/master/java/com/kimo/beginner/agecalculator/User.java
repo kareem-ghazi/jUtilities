ae0f89b114005a885cf78579897acdb09fc2627b
Kareem Ghazi <k.ghazi2005@gmail.com>
2021-08-07 19:49:05 +0300

    Catch errors for creating a user

--- src/master/java/com/kimo/beginner/agecalculator/User.java
+++ src/master/java/com/kimo/beginner/agecalculator/User.java
@@ -6,15 +6,19 @@ import java.util.Scanner;
 public class User {
 	private String username;
 	private String nationality;
-	
+
 	private int birthYear;
 	private int birthMonth;
 	private int birthDay;
 
 	private LocalDate birthday;
 	private Scanner scan = new Scanner(System.in);
-	
+
 	public void createUser() {
+		int birthYear = 0;
+		int birthMonth = 0;
+		int birthDay = 0;
+
 		System.out.print("Enter your name: ");
 		String username = scan.nextLine();
 
@@ -22,13 +26,42 @@ public class User {
 		String nationality = scan.nextLine();
 
 		System.out.print("Enter your birth year: ");
-		int birthYear = scan.nextInt();
+		do {
+			try {
+				birthYear = scan.nextInt();
+				break;
+			} catch (Exception e) {
+				scan.nextLine();
+				System.out.println("Invalid input.");
+				System.out.print("-: ");
+			}
+		} while (true);
+		scan.nextLine();
 
 		System.out.print("Enter your birth month: ");
-		int birthMonth = scan.nextInt();
+		do {
+			try {
+				birthMonth = scan.nextInt();
+				break;
+			} catch (Exception e) {
+				scan.nextLine();
+				System.out.println("Invalid input.");
+				System.out.print("-: ");
+			}
+		} while (true);
+		scan.nextLine();
 
 		System.out.print("Enter your birth day: ");
-		int birthDay = scan.nextInt();
+		do {
+			try {
+				birthDay = scan.nextInt();
+				break;
+			} catch (Exception e) {
+				scan.nextLine();
+				System.out.println("Invalid input.");
+				System.out.print("-: ");
+			}
+		} while (true);
 		scan.nextLine();
 
 		setName(username);
@@ -38,36 +71,36 @@ public class User {
 
 	public void setName(String username) {
 		this.username = username;
-		
+
 	}
-	
+
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
-		
+
 	}
-	
+
 	public void setBirthdate(int birthYear, int birthMonth, int birthDay) {
 		this.birthYear = birthYear;
 		this.birthMonth = birthMonth;
 		this.birthDay = birthDay;
-		
+
 		birthday = LocalDate.of(birthYear, birthMonth, birthDay);
 	}
 
 	public boolean isNull() {
 		return getUsername() == null;
 	}
-	
+
 	public String getUsername() {
 		return username;
-		
+
 	}
-	
+
 	public String getNationality() {
 		return nationality;
-		
+
 	}
-	
+
 	public LocalDate getBirthdate() {
 		return birthday;
 
@@ -75,17 +108,17 @@ public class User {
 
 	public int getBirthMonth() {
 		return birthMonth;
-		
+
 	}
-	
+
 	public int getBirthYear() {
 		return birthYear;
-		
+
 	}
-	
+
 	public int getBirthDay() {
 		return birthDay;
-		
+
 	}
-	
+
 }
