51df6ba9e23ef52f174c5f04e39e8bc609ceedd9
Kareem Ghazi <k.ghazi2005@gmail.com>
2021-09-04 18:26:18 +0300

    CollatzConjecture: Initial commit

--- src/com/kimo/beginner/collatzconjecture/CollatzConjecture.java
+++ src/com/kimo/beginner/collatzconjecture/CollatzConjecture.java
@@ -3,23 +3,66 @@ package com.kimo.beginner.collatzconjecture;
 import java.util.Scanner;
 
 public class CollatzConjecture {
-	static Scanner scan = new Scanner(System.in);
-	
+	private static Scanner scan = new Scanner(System.in);
+	private static int i = 0; // Steps counter
+
 	public static void main(String[] args) {
-		System.out.println("Collatz Conjecture: ");
+		System.out.println("Collatz Conjecture - Algorithm");
+		System.out.println("--------------------------------------");
 		System.out.print("Enter a number for the algorithm: ");
-		int userInput = scan.nextInt();
-		
-		int i;
-		for (i = 1; userInput != 1; i++) {
-			if (userInput % 2 == 0) {
-				userInput = userInput / 2;
-			} else if (userInput % 2 != 0) {
-				userInput = userInput * 3 + 1;
+
+		// Main loop.
+		while (true) {
+			String userInput = scan.nextLine();
+
+			if (userInput.equals("!q")) {
+				System.out.println("Exited program successfully.");
+				System.out.println("--------------------------------------");
+				break;
 			}
+
+			i = 0;
+			int steps = 0;
+
+			if (isNumeric(userInput) && Integer.parseInt(userInput) != 0) {
+				steps = collatzConjecture(Integer.parseInt(userInput));
+				System.out.println("Amount of steps to reach 1: " + steps);
+			} else {
+				System.out.println("Invalid input.");
+			}
+
+			System.out.print("-: ");
 		}
-		
-		System.out.println("Amount of steps to reach one: " + i);
+
 	}
 
+	// Collatz Conjecture for the specified number.
+	// --------------------------------------
+	// If the number is even, it is divided by 2.
+	// If the number is odd, it is multiplied by 3 and then added with 1.
+	private static int collatzConjecture(int number) {
+		if (number != 1) {
+			if (number % 2 == 0) {
+				number = number / 2;
+				i++;
+			} else if (number % 2 != 0) {
+				number = number * 3 + 1;
+				i++;
+			}
+		} else if (number == 1) {
+			return i;
+		}
+
+		return collatzConjecture(number);
+	}
+
+	// Checks if the input is numeric.
+	private static boolean isNumeric(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 }
---
+++ src/com/kimo/beginner/collatzconjecture/README.md
@@ -0,0 +1,11 @@
+# CollatzConjecture #
+
+Collatz conjecture algorithm.
+
+## What is this? ##
+
+A simple algorithm using a number as input. If the specified number is even, it divides it by 2. If the specified number is odd, it multiplies it by 3 and then add 1. This is repeated until the number reaches a value of 1. This program has the feature of returning the amount of steps it took to reach 1 and printing it out. 
+
+## How to use? ##
+
+Enter a valid number to run it through the algorithm. You can type `!q` anytime to exit the program.
